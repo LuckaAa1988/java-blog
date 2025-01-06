bfbff267bc266a6e2b86b6229360d6ee5bde5362
Luckaaa <perepelitsalesha@yandex.ru>
2025-01-06 18:43:25 +0300

    добавлен логгер и вынесены данные для подключения к БД в отдельный файл

--- src/main/java/ru/practicum/configuration/DataSourceConfig.java
+++ src/main/java/ru/practicum/configuration/DataSourceConfig.java
@@ -1,22 +1,34 @@
 package ru.practicum.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import javax.sql.DataSource;
 
 @Configuration
+@PropertySource("classpath:application.properties")
 public class DataSourceConfig {
+
+    @Value("${datasource.driver}")
+    private String driver;
+    @Value("${datasource.url}")
+    private String url;
+    @Value("${datasource.user}")
+    private String user;
+    @Value("${datasource.password}")
+    private String password;
 
     @Bean
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("org.postgresql.Driver");
-        dataSource.setUrl("jdbc:postgresql://db:5432/blogdb");
-        dataSource.setUsername("user");
-        dataSource.setPassword("12345");
+        dataSource.setDriverClassName(driver);
+        dataSource.setUrl(url);
+        dataSource.setUsername(user);
+        dataSource.setPassword(password);
         return dataSource;
     }
 
--- src/main/java/ru/practicum/service/impl/PostServiceImpl.java
+++ src/main/java/ru/practicum/service/impl/PostServiceImpl.java
@@ -1,13 +1,13 @@
 package ru.practicum.service.impl;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import ru.practicum.dto.CreatePostDTO;
 import ru.practicum.dto.FullPostDTO;
 import ru.practicum.dto.PreviewPostDTO;
 import ru.practicum.mapper.PostMapper;
-import ru.practicum.model.Post;
 import ru.practicum.repository.PostRepository;
 import ru.practicum.service.PostService;
 
@@ -17,6 +17,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class PostServiceImpl implements PostService {
@@ -25,6 +26,7 @@ public class PostServiceImpl implements PostService {
     private final PostMapper postMapper;
 
     public List<PreviewPostDTO> findAllPosts() throws SQLException {
+        log.info("Запрос на получение всех постов");
         return postRepository.findAllPosts().stream()
                 .map(postMapper::toPreviewDTO)
                 .collect(Collectors.toList());
@@ -32,6 +34,7 @@ public class PostServiceImpl implements PostService {
 
     @Override
     public List<PreviewPostDTO> findAllPostsWithTag(String tag) {
+        log.info("Запрос на получение всех постов с тегом: {}", tag);
         return postRepository.findAllPostsWithTag(tag).stream()
                 .map(postMapper::toPreviewDTO)
                 .collect(Collectors.toList());
@@ -39,6 +42,7 @@ public class PostServiceImpl implements PostService {
 
     @Override
     public void updatePost(Long postId, String name, String text, String tags, MultipartFile image) throws IOException {
+        log.info("Обновление поста с id {}", postId);
         var post = postRepository.findById(postId).get();
         post.setId(postId);
         if (name != null) {
@@ -61,6 +65,7 @@ public class PostServiceImpl implements PostService {
 
     @Override
     public FullPostDTO findById(Long postId) {
+        log.info("Получение поста с id {}", postId);
         var post = postRepository.findById(postId).get();
         var comments = postRepository.findAllCommentsByPostId(postId).reversed();
         return postMapper.toFullDTO(post, comments);
@@ -68,31 +73,37 @@ public class PostServiceImpl implements PostService {
 
     @Override
     public void createPost(CreatePostDTO postDTO) {
+        log.info("Создание поста с данными: {}", postDTO);
         postRepository.createPost(postMapper.fromDTO(postDTO));
     }
 
     @Override
     public Integer addLike(Long postId) {
+        log.info("Добалвние лайка к посту с id {}", postId);
         return postRepository.addLike(postId);
     }
 
     @Override
     public void deletePost(Long postId) {
+        log.info("Удаление поста с id {}", postId);
         postRepository.deletePost(postId);
     }
 
     @Override
     public String addComment(Long postId, String text) {
+        log.info("Добавление комментария к посту с id {}", postId);
         return postRepository.addComment(postId, text);
     }
 
     @Override
     public void updateComment(Long commentId, String text) {
+        log.info("Обновление комментария с id {}", commentId);
         postRepository.updateComment(commentId, text);
     }
 
     @Override
     public void deleteComment(Long commentId) {
+        log.info("Удаление комментария с id {}", commentId);
         postRepository.deleteComment(commentId);
     }
 }
